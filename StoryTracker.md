de58521e0a86739f3ef2d8aeb95ca9e7a4d20af7
Mostafa Habib <mostafa.habib@mapp.com>
2018-12-15 18:48:59 -0500

    create single page with text fields and submit button with material ui

--- StoryTracker.md
+++ StoryTracker.md
@@ -4,12 +4,9 @@
 Simple app that calculates the cost of a company by the salaries of its employees. Single page application form for inputting employee data and another form to get company cost of a selected company.
 
 ## Stories to be completed:
-* add material ui to make it look pretty
-* add mutations to front end
-* create single page with text fields on the left and submit button
-* implement display on the right from database 
+* implement table for display, edit and delete
+* create query for company cost calculation
 * implement search and display of company costs
-* implement search, update and delete functionality to original single page
 * add front end unit tests
 * add back end unit tests
 * Push GitHub repository to a cloud hosting provider
@@ -25,4 +22,5 @@ Simple app that calculates the cost of a company by the salaries of its employee
 * db level create and read functionality.
 * db level update and delete functionality based on employee id.
 * create front end react app with basic read functionality to show employee first name
+* create single page with text fields and submit button with material ui
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import { graphql, compose } from "react-apollo";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const EmployeesQuery =
-gql`{
+const EmployeesQuery = gql`
+{
   employees {
     employeeId
     firstName
@@ -13,16 +15,156 @@ gql`{
     salary
   }
 }`;
+const CreateEmployeeMutation = gql`
+  mutation(
+    $employeeId: Int!, 
+    $firstName: String!, 
+    $lastName: String!, 
+    $address: String, 
+    $company: String!, 
+    $salary: Int!) {
+    createEmployee(
+      employeeId: $employeeId,
+      firstName: $firstName,
+      lastName: $lastName,
+      address: $address,
+      company: $company,
+      salary: $salary) {
+        employeeId
+        firstName
+        lastName
+        address
+        company
+        salary
+    }
+  }
+`;
+
+
 class App extends Component {
+  state = {
+    employeeId: 0,
+    firstName: "",
+    lastName: "",
+    address: "",
+    company: "",
+    salary: 0
+  };
+
+  handleEmployeeIdChange = e => {
+    const newEmployeeId = parseInt(e.target.value);
+    this.setState({
+      employeeId: newEmployeeId,
+    });
+  };
+
+  handleFirstNameChange = e => {
+    const newFirstName = e.target.value;
+    this.setState({
+        firstName: newFirstName,
+    });
+  };
+
+  handleLastNameChange = e => {
+    const newLastName = e.target.value;
+    this.setState({
+        lastName: newLastName,
+    });
+  };
+
+  handleAddressChange = e => {
+    const newAddress = e.target.value;
+    this.setState({
+        address: newAddress,
+    });
+  };
+
+  handleCompanyChange = e => {
+    const newCompany = e.target.value;
+    this.setState({
+        company: newCompany,
+    });
+  };
+  
+  handleSalaryChange = e => {
+    const newSalary = parseInt(e.target.value);
+    this.setState({
+        salary: newSalary,
+    });
+  };
+  
+  createEmployee = async employee => {
+    await this.props.createEmployee({
+      variables: {
+        employeeId: employee.employeeId,
+        firstName: employee.firstName,
+        lastName: employee.lastName,
+        address: employee.address,
+        company: employee.company,
+        salary: employee.salary
+      },
+      update: (store, { data: { createEmployee } }) => {
+        // Read the data from our cache for this query.
+        const data = store.readQuery({ query: EmployeesQuery });
+        // Add our text from the mutation to the top.
+        data.employees.unshift(createEmployee)
+        // Write our data back to the cache.
+        store.writeQuery({ query: EmployeesQuery, data });
+      }
+    });
+  };
+
   render() {
-    const {data: {loading, employees}} = this.props;
+    const {data: {loading}} = this.props;
     if (loading) {
       return null;
     }
-    return <div>
-        {employees.map(employee => (<div key={`${employee.employeeId}-employee`}>{employee.firstName}</div>))}
-      </div>;
-  }
-}
+    return (
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "auto", width: 400 }}>
+            <TextField
+            onChange={this.handleEmployeeIdChange}
+            label="Employee Id"
+            margin="normal"
+            type="number"
+            fullWidth/>
+            <TextField
+            onChange={this.handleFirstNameChange}
+            label="First Name"
+            margin="normal"
+            fullWidth/>
+            <TextField
+            onChange={this.handleLastNameChange}
+            label="Last Name"
+            margin="normal"
+            fullWidth/>
+            <TextField
+            onChange={this.handleAddressChange}
+            label="Address"
+            margin="normal"
+            fullWidth/>
+            <TextField
+            onChange={this.handleCompanyChange}
+            label="Company"
+            margin="normal"
+            fullWidth/>
+            <TextField
+            onChange={this.handleSalaryChange}
+            label="Salary"
+            margin="normal"
+            type="number"
+            fullWidth/>
+            <Button variant="contained" type="submit" onClick={() => this.createEmployee(this.state)}>
+            Submit
+            </Button>
+          </div>
+        </div>
+    );
+      }
+    }
 
-export default graphql(EmployeesQuery)(App);
+export default compose(
+  graphql(CreateEmployeeMutation, { name: "createEmployee"}),
+  graphql(EmployeesQuery)
+)(App);
+
